afc5aed604dd046a82d7adb07745013afde9f941
Pedro Plasencia <pjpv9011@gmail.com>
2021-10-10 21:07:28 +0200

    feat: added strict false for model validation

--- models/saints.js
+++ models/saints.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 
-const Saints = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const Saints = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    star: {
+      type: String,
+      required: true,
+    },
+    armour: {
+      type: String,
+      required: true,
+    },
   },
-  star: {
-    type: String,
-    required: true,
-  },
-  armour: {
-    type: String,
-    required: true,
-  },
-});
+  { strict: false }
+);
 
 const Saint = mongoose.model("Saints", Saints);
 
